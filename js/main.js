27263374d0f35f3c4c919b452e446050fb0a5f43
Daniel Hearn <DanielHearn@users.noreply.github.com>
2017-10-13 01:03:14 +0100

    Add smooth anchor scrolling

--- js/main.js
+++ js/main.js
@@ -49,3 +49,16 @@ var list = new Vue({
     ],
   }
 })
+
+var nav = document.querySelector('.nav');
+window.onscroll = function () {
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+        nav.classList.add("nav-colored");
+        nav.classList.remove("nav-transparent");
+    }
+    else {
+        nav.classList.add("nav-transparent");
+        nav.classList.remove("nav-colored");
+    }
+};
