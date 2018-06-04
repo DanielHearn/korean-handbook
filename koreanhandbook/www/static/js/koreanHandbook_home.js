ac939e47d0df06b333eb7f1ef626972ad2f5e4d9
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-06-04 18:46:41 +0100

    Fix static links

--- koreanhandbook/www/static/js/koreanHandbook_home.js
+++ koreanhandbook/www/static/js/koreanHandbook_home.js
@@ -1,8 +1,21 @@
-const menuButton = document.querySelector(".button--overlay")
+const menuButton = document.querySelector('.button--overlay')
 menuButton.addEventListener('click', overlay)
 
 function overlay () {
-  document.querySelector(".overlay").classList.toggle("active");
-  menuButton.classList.toggle("active");
-  document.querySelector("body").classList.toggle("noscroll");
+  document.querySelector('.overlay').classList.toggle('active')
+  menuButton.classList.toggle('active')
+  document.querySelector('body').classList.toggle('noscroll')
 }
+
+const siema = new Siema({
+  selector: '.jsSeimaSlider',
+  duration: 750,
+  easing: 'ease-out',
+  loop: true
+})
+const prevButton = document.querySelector('.slider__prev')
+const nextButton = document.querySelector('.slider__next')
+setInterval(() => siema.next(), 3000)
+
+prevButton.addEventListener('click', () => siema.prev())
+nextButton.addEventListener('click', () => siema.next())
