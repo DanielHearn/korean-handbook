0453ca7b5abbb215352c2c8e09e0897b9d8f854e
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-06-05 15:21:34 +0100

    Fix css linkage for adblock css fix

--- koreanhandbook/www/static/js/koreanHandbook_home.js
+++ koreanhandbook/www/static/js/koreanHandbook_home.js
@@ -1,25 +1,41 @@
 const menuButton = document.querySelector('.button--overlay')
 menuButton.addEventListener('click', overlay)
+const sliderContainer = document.querySelector('.jsSeimaSlider')
 
 function overlay () {
   document.querySelector('.overlay').classList.toggle('active')
   menuButton.classList.toggle('active')
   document.querySelector('body').classList.toggle('noscroll')
 }
-
+let intervalID = null
+let mouseDownIntervalID = null
 const siema = new Siema({
   selector: '.jsSeimaSlider',
   duration: 1000,
   easing: 'ease-out',
-  threshold: 500,
+  threshold: 200,
   loop: true,
-  onChange: () => sliderAutoPlay(),
-  onInit: () => sliderAutoPlay()
+  onChange: sliderAutoPlay
 })
 const prevButton = document.querySelector('.slider__prev')
 const nextButton = document.querySelector('.slider__next')
+sliderAutoPlay()
 function sliderAutoPlay () {
-  setTimeout(() => siema.next(), 3000)
+  clearSliderAutoPlay()
+  intervalID = setInterval(() => siema.next(), 5000)
+}
+function clearSliderAutoPlay () {
+  clearInterval(intervalID)
+}
+function mouseDownInterval () {
+  clearSliderAutoPlay()
+  clearMouseDownInterval()
+  mouseDownIntervalID = setInterval(() => mouseDownInterval(), 200)
+}
+function clearMouseDownInterval () {
+  clearInterval(mouseDownIntervalID)
 }
 prevButton.addEventListener('click', () => siema.prev())
 nextButton.addEventListener('click', () => siema.next())
+sliderContainer.addEventListener('mousedown', () => mouseDownInterval())
+sliderContainer.addEventListener('mouseup', () => clearMouseDownInterval())
