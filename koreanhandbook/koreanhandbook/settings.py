501afba8e9e1c642e64be3fd927f8257c8f1f01b
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-06-06 15:00:44 +0100

    Add heading to home content

--- koreanhandbook/koreanhandbook/settings.py
+++ koreanhandbook/koreanhandbook/settings.py
@@ -131,7 +131,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 3000
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
