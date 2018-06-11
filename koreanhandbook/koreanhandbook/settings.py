4ec016c62094c40399f16e276d0d6209605d2363
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-06-11 15:19:24 +0100

    Move page feedback button to above content

--- koreanhandbook/koreanhandbook/settings.py
+++ koreanhandbook/koreanhandbook/settings.py
@@ -15,10 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
 
 if 'SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ['SECRET_KEY']
