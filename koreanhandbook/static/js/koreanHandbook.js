85f60195810c6e1076c39bfa8d3b26c012dc33b7
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-10-08 15:20:52 +0100

    Revert new design

--- koreanhandbook/static/js/koreanHandbook.js
+++ koreanhandbook/static/js/koreanHandbook.js
@@ -67,53 +67,59 @@ function search (tags, searchText) {
 }
 
 // Korean Clock
-if (document.querySelector('#clock')) {
-  function getTime (date) {
-    const minutes = `0${date.getMinutes()}`.slice(-2)
-    let hours
-    if (date.getHours() <= 12) {
-      hours = date.getHours()
-    } else {
-      hours = date.getHours() - 12
+try {
+  if (document.querySelector('#clock')) {
+    let hours24 = true
+
+    function getTime (date) {
+      const minutes = `0${date.getMinutes()}`.slice(-2)
+      let hours
+      if (date.getHours() <= 12) {
+        hours = date.getHours()
+      } else {
+        hours = date.getHours() - 12
+      }
+      const time = `${hours}:${minutes}`
+      return time
     }
-    const time = `${hours}:${minutes}`
-    return time
-  }
 
-  function getDay (date) {
-    const days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
-    const dayNumber = date.getDay()
-    return days[dayNumber]
-  }
-
-  function getMonth (date) {
-    return date.getMonth() + 1
-  }
-
-  function displayClock () {
-    const date = new Date()
-    const time = getTime(date)
-    const day = getDay(date)
-    const month = getMonth(date)
-    const hours = date.getHours()
-    const shortDate = `${date.getFullYear()}년 ${month}월 ${date.getDate()}일 ${day}`
-    let prefix
-    if (hours > 12) {
-      prefix = '오후'
-    } else {
-      prefix = '오전'
+    function getDay (date) {
+      const days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
+      const dayNumber = date.getDay()
+      return days[dayNumber]
     }
-    document.querySelector('#prefix').textContent = prefix
-    document.querySelector('#time').textContent = time
-    document.querySelector('#date').textContent = shortDate
-  }
 
-  function startClock () {
-    displayClock()
-    setInterval(displayClock, 1000)
-  }
+    function getMonth (date) {
+      return date.getMonth() + 1
+    }
 
-  startClock()
+    function displayClock () {
+      const date = new Date()
+      const time = getTime(date)
+      const day = getDay(date)
+      const month = getMonth(date)
+      const hours = date.getHours()
+      const shortDate = `${date.getFullYear()}년 ${month}월 ${date.getDate()}일 ${day}`
+      let prefix
+      if (hours > 12) {
+        prefix = '오후'
+      } else {
+        prefix = '오전'
+      }
+      document.querySelector('#prefix').textContent = prefix
+      document.querySelector('#time').textContent = time
+      document.querySelector('#date').textContent = shortDate
+    }
+
+    function startClock () {
+      displayClock()
+      setInterval(displayClock, 1000)
+    }
+
+    startClock()
+  }
+} catch (e) {
+
 }
 
 // ADS
