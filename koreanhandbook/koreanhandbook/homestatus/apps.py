94261952f6df490df274c66d315e4251d7c79752
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-10-09 10:39:57 +0100

    Add homestatus init and apps.py

---
+++ koreanhandbook/koreanhandbook/homestatus/apps.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+# Assign the core of the Korean Handbook app
+class HomestatusConfig(AppConfig):
+    name = 'homestatus'
