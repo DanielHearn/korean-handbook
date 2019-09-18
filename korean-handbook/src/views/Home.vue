9c553a1bd00172a2f49e9c246a7ce16658c9e37c
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-09-18 16:34:37 +0100

    Add links and intro to home

--- korean-handbook/src/views/Home.vue
+++ korean-handbook/src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="main home">
-    <h1>Home</h1>
+    <div class="intro">
+      <h1>The Korean Handbook</h1>
+      <h2>Collection of Korean language learning tools and information.</h2>
+    </div>
+    <div class="intro-links">
+      <ul>
+        <li>
+          <router-link to="/random-words/all">Random Words</router-link>
+        </li>
+        <li>
+          <router-link to="/info/">Info</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
