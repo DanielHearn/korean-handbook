ebe08417a3720779d00a037c82a12e79fb319093
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-10-03 09:57:51 +0100

    Add home page

---
+++ korean-handbook/src/static/updates.js
@@ -0,0 +1,14 @@
+export const Updates = [
+  {
+    date: '4rd October 2019',
+    description: 'Add new home page design.'
+  },
+  {
+    date: '3rd October 2019',
+    description: 'Add dynamic titles'
+  },
+  {
+    date: '2rd October 2019',
+    description: 'Started new design'
+  }
+]
--- korean-handbook/src/views/Home.vue
+++ korean-handbook/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="main home">
-    <div class="intro">
-      <h1>The Korean Handbook</h1>
-      <h2>Collection of Korean language learning tools and information.</h2>
-    </div>
-    <div class="intro-links">
-      <ul>
-        <li>
-          <router-link to="/random-words/all">Random Words</router-link>
-        </li>
-        <li>
-          <router-link to="/info/">Info</router-link>
-        </li>
-      </ul>
+    <div class="home-content">
+      <div class="intro">
+        <h1>The Korean Handbook</h1>
+        <h2>Collection of Korean language learning tools and information.</h2>
+      </div>
+      <div class="intro-links">
+        <ul>
+          <li class="sub-heading">
+            <router-link to="/random-words/all">Random Words</router-link>
+          </li>
+          <li class="sub-heading">
+            <router-link to="/info/">Info</router-link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p class="heading">Updates</p>
+        <ul class="update-list">
+          <li v-for="update in $options.updates" :key="update.date">
+            <p class="sub-heading">{{ update.date }}</p>
+            <p>{{ update.description }}</p>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
+
+
+<script>
+import { Updates } from "./../static/updates.js";
+
+export default {
+  name: "home",
+  updates: Updates
+};
+</script>
