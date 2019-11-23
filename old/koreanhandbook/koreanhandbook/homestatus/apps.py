b67d349253652dfe6fbfac2627b7c757a76ac34f
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-11-23 17:23:11 +0000

    Remove old

--- old/koreanhandbook/koreanhandbook/homestatus/apps.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-# Assign the core of the Korean Handbook app
-class HomestatusConfig(AppConfig):
-    name = 'homestatus'
--- old/koreanhandbook/src/js/quiz.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import Quiz from './quiz/quiz.vue'
-
-const quizApp = new Vue({
-  el: '#app',
-  render: h => h(Quiz)
-})
