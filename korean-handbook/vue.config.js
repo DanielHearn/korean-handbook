21e8d50f94e9be304dc658775125c39770656d91
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-11-23 17:52:45 +0000

    Disable pwa

--- korean-handbook/vue.config.js
+++ korean-handbook/vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  pwa: {
+    serviceWorker:false,
+    workboxPluginMode:'InjectManifest',
+    workboxOptions: {
+      swSrc: './app/sw.js', /* Empty file. */
+    }
+  }
 }
