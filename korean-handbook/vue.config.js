25729dd88fa00c9828c54bcd91ce5da3057fc3d6
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-11-23 18:46:46 +0000

    Fix build

--- korean-handbook/vue.config.js
+++ korean-handbook/vue.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   pwa: {
-    serviceWorker:false,
-    workboxPluginMode:'InjectManifest',
-    workboxOptions: {
-      swSrc: './app/sw.js', /* Empty file. */
-    }
+    serviceWorker: false
   }
 }
