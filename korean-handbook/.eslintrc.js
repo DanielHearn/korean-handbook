ac6ee2a012d1e99613a33007a8680f97f295ed05
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-11-24 14:29:31 +0000

    Fix semi colons in eslint

--- korean-handbook/.eslintrc.js
+++ korean-handbook/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    semi: 0,
+    semi: [2, 'never'],
     quotes: [0],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
