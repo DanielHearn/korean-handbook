01a47c2c2759951c58f9ee07f30a88016890e776
Daniel Hearn <DanielHearn@users.noreply.github.com>
2022-03-08 20:01:30 +0000

    Add new random UI to match design

--- korean-handbook/src/components/RandomWordGenerator/RandomWordGenerator.vue
+++ korean-handbook/src/components/RandomWordGenerator/RandomWordGenerator.vue
@@ -1,88 +1,88 @@
 <template>
   <div class="random">
-    <div class="random-options">
-      <p class="sub-heading">Options</p>
-      <div class="random-option">
-        <input
-          id="flashcardModeToggle"
-          type="checkbox"
-          v-model="flashcardMode"
-        />
-        <label for="flashcardModeToggle" class="text">Flashcard Mode</label>
-      </div>
-      <div v-if="flashcardMode">
-        <p class="text">
-          Select the language direction for the flashcards.
-        </p>
-        <div>
-          <input
-            id="englishDirectionToggle"
-            type="radio"
-            value="toEnglish"
-            v-model="languageDirection"
-          />
-          <label for="englishDirectionToggle" class="text"
-            >Korean To English</label
-          >
-        </div>
-        <div>
-          <input
-            id="koreanDirectionToggle"
-            type="radio"
-            value="toKorean"
-            v-model="languageDirection"
-          />
-          <label for="koreanDirectionToggle" class="text"
-            >English To Korean</label
-          >
-        </div>
-      </div>
-    </div>
-    <div
-      v-if="flashcardMode"
-      class="random-flashcards random-flashcards--test"
-      @click="showAnswer = true"
-    >
-      <div class="random-flashcard random-flashcard--light">
-        <p v-if="languageDirection === 'toEnglish'" class="text">
-          {{ word.k }}
-        </p>
-        <p v-else class="text">{{ word.e }}</p>
-      </div>
-      <div class="random-flashcard random-flashcard--dark">
-        <template v-if="showAnswer">
-          <p v-if="languageDirection === 'toEnglish'" class="text">
-            {{ word.e }}
-          </p>
-          <p v-else class="text">{{ word.k }}</p>
-        </template>
-        <template v-else>
-          <p class="text">
-            Click for the translation
-          </p>
-        </template>
-      </div>
-    </div>
-    <div v-else class="random-flashcards">
-      <div class="random-flashcard random-flashcard--light">
-        <p class="text">{{ word.k }}</p>
-      </div>
-      <div class="random-flashcard random-flashcard--dark">
-        <p class="text">{{ word.e }}</p>
-      </div>
-    </div>
-    <div class="actions">
-      <button class="button--primary" @click="generateWord">
-        Next Word
-      </button>
-      <button
-        class="button--secondary"
+    <div class="random-content">
+      <div
         v-if="flashcardMode"
-        :disabled="showAnswer"
+        class="random-flashcards random-flashcards--test"
         @click="showAnswer = true"
       >
-        Show Answer
-      </button>
+        <div class="random-flashcard random-flashcard--light">
+          <p v-if="languageDirection === 'toEnglish'" class="text">
+            {{ word.k }}
+          </p>
+          <p v-else class="text">{{ word.e }}</p>
+        </div>
+        <div class="random-flashcard random-flashcard--dark">
+          <template v-if="showAnswer">
+            <p v-if="languageDirection === 'toEnglish'" class="text">
+              {{ word.e }}
+            </p>
+            <p v-else class="text">{{ word.k }}</p>
+          </template>
+          <template v-else>
+            <p class="text">
+              Click for the translation
+            </p>
+          </template>
+        </div>
+      </div>
+      <div v-else class="random-flashcards">
+        <div class="random-flashcard random-flashcard--light">
+          <p class="text">{{ word.k }}</p>
+        </div>
+        <div class="random-flashcard random-flashcard--dark">
+          <p class="text">{{ word.e }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="random-options">
+      <div>
+        <div class="random-option">
+          <input
+            id="flashcardModeToggle"
+            type="checkbox"
+            v-model="flashcardMode"
+          />
+          <label for="flashcardModeToggle" class="text">Flashcard Mode</label>
+        </div>
+        <div v-if="flashcardMode">
+          <div>
+            <input
+              id="englishDirectionToggle"
+              type="radio"
+              value="toEnglish"
+              v-model="languageDirection"
+            />
+            <label for="englishDirectionToggle" class="text"
+              >Korean To English</label
+            >
+          </div>
+          <div>
+            <input
+              id="koreanDirectionToggle"
+              type="radio"
+              value="toKorean"
+              v-model="languageDirection"
+            />
+            <label for="koreanDirectionToggle" class="text"
+              >English To Korean</label
+            >
+          </div>
+        </div>
+      </div>
+      <div class="actions">
+        <button class="button--primary" @click="generateWord">
+          Next Word
+        </button>
+        <button
+          class="button--secondary"
+          v-if="flashcardMode"
+          :disabled="showAnswer"
+          @click="showAnswer = true"
+        >
+          Show Answer
+        </button>
+      </div>
     </div>
   </div>
 </template>
