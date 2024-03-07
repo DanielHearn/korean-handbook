673a4a3d735f68d587161d681dd302e2aff33474
Daniel Hearn <DanielHearn@users.noreply.github.com>
2024-03-07 21:56:59 +0000

    Add workflow

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../views/HomePage/HomePage.vue';
 import { Categories } from '@/static/categories';
 
@@ -110,7 +110,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
