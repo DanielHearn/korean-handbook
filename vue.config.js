ce67eff09f508b037cde66016ec369fec8b62947
Daniel Hearn <DanielHearn@users.noreply.github.com>
2024-03-08 19:33:55 +0000

    Fix public path in vue

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/korean-handbook/' : '/',
 });
